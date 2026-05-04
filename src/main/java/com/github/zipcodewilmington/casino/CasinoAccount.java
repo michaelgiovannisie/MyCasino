e3fb4a05e7c04bde67fd5e1f38e0e4abf6364f50
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-05-04 15:26:45 -0400

    casino account and casinoaccountmanager updated

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -6,4 +6,29 @@ package com.github.zipcodewilmington.casino;
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+
+    private String accountName;
+    private String accountPassword;
+    private double accountBalance;
+
+    public CasinoAccount(String accountName, String accountPassword) {
+        this.accountName = accountName;
+        this.accountPassword = accountPassword;
+        this.accountBalance = 1000;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+
+
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,7 +1,65 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
-/**
- * Created by leon on 7/21/2020.
- */
-public class NumberGuessGame {
+import java.util.Random;
+import java.util.Scanner;
+
+import com.github.zipcodewilmington.casino.GameInterface;
+
+public class NumberGuessGame implements GameInterface {
+
+    private int number;
+    private int guesses;
+    private static final int MAX_GUESSES = 7;
+
+    public NumberGuessGame() {
+        Random random = new Random();
+        this.number = random.nextInt(100) + 1;
+        this.guesses = 0;
+    }
+
+    public void play(Scanner scanner) {
+        printGameStart();
+        while (guesses < MAX_GUESSES) {
+
+                System.out.println("Enter your guess: ");
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. Please enter a number.");
+                    scanner.next();
+                    continue;
+                }
+
+                int guess = scanner.nextInt();
+                if (guess < 1 || guess > 100) {
+                    System.out.println("Please enter a number between 1 and 100.");
+                    continue;
+                }
+
+                this.guesses++;
+
+                if (guess == this.number) {
+                    System.out.println("Congratulations! You guessed the number " + this.number + " in " + this.guesses + " guesses!");
+                    break;
+                }
+
+                else if (guess < this.number) {
+                    System.out.println("Too low! You have " + (MAX_GUESSES - this.guesses) + " guesses left.");
+                }
+
+                else if (guess > this.number) {
+                    System.out.println("Too high! You have " + (MAX_GUESSES - this.guesses) + " guesses left.");
+                }
+
+                if (guesses >= MAX_GUESSES) {
+                    System.out.printf("Sorry, you've run out of guesses! The number was " + this.number + ".");
+                    System.out.println("The secret number was: " + this.number);
+                    break;
+                }
+        }
+    }
+
+    public void printGameStart() {
+        System.out.println("Welcome to the Number Guess Game! A number between 1 and 100 is randomly chosen. Can you guess it?");
+        System.out.println("You have " + MAX_GUESSES + " guesses. Good luck!");
+    }
+
 }
