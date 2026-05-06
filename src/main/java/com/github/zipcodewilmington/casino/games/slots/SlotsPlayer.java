3afe947385278d4e2d0015e48481044bd8bb2d60
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-05-06 13:36:05 -0400

    minor

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -7,9 +7,6 @@ import java.util.Random;
 import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
-/**
- * Created by leon on 7/21/2020.
- */
 public class SlotsPlayer implements PlayerInterface {
 
    private CasinoAccount casinoAccount;
